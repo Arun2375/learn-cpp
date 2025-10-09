5b7c9574fe446756701a762993f84671bd2c2bf9
Arun Singh <parihararunrajput@gmail.com>
2025-10-09 05:58:11 +0000

    add cpp

---
+++ 12th_condition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//IF STATEMENT = do something is the condition is true
+// if not, then don't do it 
+int main(){
+int age;
+std::cout<<"Enter your age: ";
+std::cin>>age;
+
+if(age >= 100){
+    Std::cout<<"You are to old to enter this site";
+}
+else if()
+}
